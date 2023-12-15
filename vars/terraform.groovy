055f309376bea480481a3ff39cfd35eb95178f5d
ThumburuAditya <chandramouliadityat@gmail.com>
2023-12-16 02:09:58 +0530

    Adding the code

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,29 +1,44 @@
 def call() {
-    pipeline{
-        agent{
-            node{
+    pipeline {
+
+        agent {
+            node {
                 label 'workstation'
             }
         }
-        parameters{
-            choice(name: 'env', choices: ['dev', 'prod'], description: 'Chose the ENV')
+
+        options {
+            ansiColor('xterm')
         }
-        stages{
-            stage('Terraform INIT'){
-                steps{
+
+        parameters {
+            choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
+            choice(name: 'action', choices: ['apply', 'destroy'], description: 'Pick Action')
+        }
+
+        stages {
+
+            stage('Terraform INIT') {
+                steps {
                     sh 'terraform init -backend-config=env-${env}/state.tfvars'
                 }
             }
-            stage('Terraform Apply'){
-                steps{
-                    sh 'terraform apply -auto-approve -var-file=env-${env}/main.tfvars'
+
+            stage('Terraform Apply') {
+                steps {
+                    sh 'terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars'
                 }
             }
+
         }
-        post{
-            always{
+
+        post {
+            always {
                 cleanWs()
             }
         }
+
     }
+
+
 }
