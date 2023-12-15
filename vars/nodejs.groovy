ece46969b2e681cad791d8d252f859d33298f2ac
ThumburuAditya <chandramouliadityat@gmail.com>
2023-12-16 03:40:43 +0530

    Adding the code

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -14,7 +14,6 @@ def call() {
 
           stage('code quality') {
               steps {
-
                   sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.42.130:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
               }
           }
