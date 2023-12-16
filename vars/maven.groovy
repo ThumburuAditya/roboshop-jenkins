d067d2ceaac0fcf35cb1881b0d0fc3f58876c69c
ThumburuAditya <chandramouliadityat@gmail.com>
2023-12-17 00:55:42 +0530

    Adding the code

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -12,15 +12,15 @@ def call() {
         }
         stages {
 
-            stage('compilation') {
+            stage('code compile') {
                 steps {
-                    sh 'echo compilation'
+                    sh 'mvn compile'
                 }
             }
 
             stage('code quality') {
                 steps {
-                    sh 'echo code quality'
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.42.130:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
                 }
             }
             stage('unit test cases') {
